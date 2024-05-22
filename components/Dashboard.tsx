1c56f53659cca55dff6d4134edd91b9710736abd
Sidharth 'R3FluX' Sreejil <94698713+sid-js@users.noreply.github.com>
2024-05-22 19:44:52 +0530

    Add Navbar and Logo

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -2,28 +2,13 @@
 
 import { Button, Flex, Heading, Icon, Text, View } from '@aws-amplify/ui-react';
 import AddDiaryDialogButton from './AddDiaryDialogButton';
+import Navbar from './Navbar';
 
 function Dashboard({ signOut }: { signOut: () => void }) {
   return (
-    <View color='#bdc3d2' backgroundColor='#000000' padding='1em 2em'>
-      <Flex justifyContent='space-between' alignItems='center'>
-        <Heading level={3}>EchoDiary</Heading>
-        <Flex gap='2em'>
-          <AddDiaryDialogButton />
-          <Button
-            padding='.2em 1em'
-            backgroundColor='transparent'
-            onClick={() => {
-              signOut();
-            }}
-          >
-            <Text fontSize='1.2rem' color='#bdc3d2'>
-              Logout
-            </Text>
-          </Button>
-        </Flex>
-      </Flex>
-    </View>
+    <main>
+      <Navbar signOut={signOut} />
+    </main>
   );
 }
 
