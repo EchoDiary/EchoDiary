a37160e4c308cbbfc9afa6dea988678de6dab486
Shivam77607 <shivamsharma77607@gmail.com>
2024-05-23 19:30:22 +0530

    feat: Add text optimisation

--- components/AddDiaryDialogButton.tsx
+++ components/AddDiaryDialogButton.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import {
   Dialog,
@@ -8,25 +8,26 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from '@/components/ui/dialog';
-import Image from 'next/image';
-import { FaMicrophone, FaRegPenToSquare } from 'react-icons/fa6';
-import { IoSparkles } from 'react-icons/io5';
-import { Textarea } from './ui/textarea';
-import { Label } from './ui/label';
-import { Button } from './ui/button';
-import { Visualizer } from 'react-sound-visualizer';
-import { useRef, useState } from 'react';
-import useMediaStream from 'use-media-stream';
+} from "@/components/ui/dialog";
+import Image from "next/image";
+import { FaMicrophone, FaRegPenToSquare } from "react-icons/fa6";
+import { IoSparkles } from "react-icons/io5";
+import { TbReload } from "react-icons/tb";
+import { Textarea } from "./ui/textarea";
+import { Label } from "./ui/label";
+import { Button } from "./ui/button";
+import { Visualizer } from "react-sound-visualizer";
+import { useRef, useState } from "react";
+import useMediaStream from "use-media-stream";
 
-import { generateClient } from 'aws-amplify/data';
-import { type Schema } from '../amplify/data/resource';
-import toast from 'react-hot-toast';
+import { generateClient } from "aws-amplify/data";
+import { type Schema } from "../amplify/data/resource";
+import toast from "react-hot-toast";
 
 const client = generateClient<Schema>();
 
 const AddDiaryDialogButton = () => {
-  const [transcription, setTranscription] = useState('');
+  const [transcription, setTranscription] = useState("");
 
   const recognitionRef = useRef<SpeechRecognition>();
   const {
@@ -40,9 +41,10 @@ const AddDiaryDialogButton = () => {
   });
 
   const [isActive, setIsActive] = useState(false);
-  const [content, setContent] = useState('');
+  const [content, setContent] = useState("");
   const [isSpeechSupported, setIsSpeechSupported] = useState(true);
   const [isOpen, setIsOpen] = useState(false);
+  const [isOptimising, setIsOptimising] = useState(false);
 
   useState(() => {
     if (!window.SpeechRecognition && !window.webkitSpeechRecognition) {
@@ -51,7 +53,7 @@ const AddDiaryDialogButton = () => {
   });
 
   function handleRecord() {
-    console.log('Recording');
+    console.log("Recording");
     if (isActive) {
       handleStop();
       setIsActive(false);
@@ -105,29 +107,61 @@ const AddDiaryDialogButton = () => {
             date: new Date().toISOString(),
           }),
           {
-            loading: 'Saving diary entry...',
-            success: 'Diary entry saved successfully',
-            error: 'Error saving diary entry',
+            loading: "Saving diary entry...",
+            success: "Diary entry saved successfully",
+            error: "Error saving diary entry",
           }
         )
         .then(() => {
-          setContent('');
+          setContent("");
           setIsOpen(false);
         })
         .finally(() => {
-          setContent('');
+          setContent("");
         });
     } catch (error) {
-      toast.error('Error saving diary entry');
-      console.error('Error saving diary entry', error);
+      toast.error("Error saving diary entry");
+      console.error("Error saving diary entry", error);
     }
   }
+
+  async function handleOptimise() {
+    if (!content) {
+      toast.error("Please enter some text to optimise");
+      return;
+    }
+
+    setIsOptimising(true);
+
+    try {
+      const response = await fetch("/api/optimise", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ text: content }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to optimize text");
+      }
+
+      const data = await response.json();
+      setContent(data.choices[0].message.content);
+    } catch (error) {
+      console.error("Error while optimizing text:", error);
+      toast.error("Failed to optimize text. Please try again later.");
+    } finally {
+      setIsOptimising(false);
+    }
+  }
+
   return (
     <Dialog
       open={isOpen}
       onOpenChange={(isOpen) => {
         if (!isOpen) {
-          setContent('');
+          setContent("");
         }
         setIsOpen(isOpen);
       }}
@@ -137,22 +171,22 @@ const AddDiaryDialogButton = () => {
           setIsOpen(true);
         }}
       >
-        <Button className='flex flex-row gap-2 items-center'>
-          {' '}
+        <Button className="flex flex-row gap-2 items-center">
+          {" "}
           <FaRegPenToSquare />
           <span>New Journal</span>
         </Button>
       </DialogTrigger>
-      <DialogContent className=''>
+      <DialogContent className="">
         <DialogHeader>
           <DialogTitle>
-            <h1 className='text-2xl font-bold'>Add a new diary entry</h1>
+            <h1 className="text-2xl font-bold">Add a new diary entry</h1>
           </DialogTitle>
         </DialogHeader>
-        <div className='flex flex-col gap-2 items-center'>
+        <div className="flex flex-col gap-2 items-center">
           {!isSupported || !isSpeechSupported ? (
             <DialogDescription>
-              <p className='text-sm text-destructive'>
+              <p className="text-sm text-destructive">
                 Your browser does not support recording audio or speech
                 recognition. Please try again with a different browser such a
                 Chrome.
@@ -160,25 +194,25 @@ const AddDiaryDialogButton = () => {
             </DialogDescription>
           ) : (
             <>
-              <div className='flex flex-row gap-4 items-start w-full'>
-                <div className='w-24 h-24 aspect-square mt-4 rounded-full animate-bounce hover:animate-none relative overflow-hidden group'>
+              <div className="flex flex-row gap-4 items-start w-full">
+                <div className="w-24 h-24 aspect-square mt-4 rounded-full animate-bounce hover:animate-none relative overflow-hidden group">
                   <Image
-                    src='/images/ai-avatar.png'
+                    src="/images/ai-avatar.png"
                     fill={true}
-                    alt='AI Avatar'
+                    alt="AI Avatar"
                   />
                 </div>
 
-                <div className='flex flex-col justify-center h-full w-fit'>
-                  <h2 className='text-2xl font-semibold'>
-                    {isActive ? 'Listening...' : "What's on your mind?"}
+                <div className="flex flex-col justify-center h-full w-fit">
+                  <h2 className="text-2xl font-semibold">
+                    {isActive ? "Listening..." : "What's on your mind?"}
                   </h2>
                   {isActive ? (
                     <Visualizer
                       autoStart
-                      mode='continuous'
+                      mode="continuous"
                       audio={stream}
-                      strokeColor='#f6ad55'
+                      strokeColor="#f6ad55"
                     >
                       {({ canvasRef }) => (
                         <>
@@ -187,49 +221,61 @@ const AddDiaryDialogButton = () => {
                       )}
                     </Visualizer>
                   ) : (
-                    <p className='text-sm whitespace-normal text-accent-foreground max-w-xs'>
-                      Start speaking to{' '}
-                      <span className='font-bold italic'>
+                    <p className="text-sm whitespace-normal text-accent-foreground max-w-xs">
+                      Start speaking to{" "}
+                      <span className="font-bold italic">
                         <span>echo &#10024;</span>
-                      </span>{' '}
+                      </span>{" "}
                       and share your thoughts by clicking the button below.
                     </p>
                   )}
                 </div>
               </div>
               <button
-                className='flex w-full items-center justify-center gap-2 bg-primary rounded-lg px-4 py-2 text-primary-foreground hover:scale-105 transition-all duration-200 hover:border-amber-500 hover:border-2'
+                className="flex w-full items-center justify-center gap-2 bg-primary rounded-lg px-4 py-2 text-primary-foreground hover:scale-105 transition-all duration-200 hover:border-amber-500 hover:border-2"
                 onClick={() => {
                   isActive ? handleStop() : handleRecord();
                 }}
               >
                 <FaMicrophone />
-                <span>{isActive ? 'Stop Listening' : 'Start Listening'}</span>
+                <span>{isActive ? "Stop Listening" : "Start Listening"}</span>
               </button>
             </>
           )}
-          <div className='grid w-full gap-3 my-2'>
-            <Label htmlFor='message'>Your thoughts</Label>
+          <div className="grid w-full gap-3 my-2">
+            <Label htmlFor="message">Your thoughts</Label>
             <Textarea
               rows={6}
-              placeholder='Today was a good day. I went to the park and played with my friends. I also had a good meal and watched a movie. I am feeling happy and content.'
-              name='message'
-              id='message'
-              className='placeholder:text-secondary-foreground/20'
+              placeholder="Today was a good day. I went to the park and played with my friends. I also had a good meal and watched a movie. I am feeling happy and content."
+              name="message"
+              id="message"
+              className="placeholder:text-secondary-foreground/20"
               value={content}
               onChange={(e) => setContent(e.target.value)}
             />
           </div>
         </div>
         <DialogFooter>
-          <Button
-            className='flex flex-row gap-2 items-center'
-            variant='outline'
-          >
-            <span>Optimize with AI</span>
-            <IoSparkles className='text-primary' />
-          </Button>
-          <Button onClick={handleSave} className='w-full'>
+          {!isOptimising ? (
+            <Button
+              className="flex flex-row gap-2 items-center"
+              variant="outline"
+              onClick={handleOptimise}
+            >
+              <span>Optimize with AI</span>
+              <IoSparkles className="text-primary" />
+            </Button>
+          ) : (
+            <Button
+              className="flex flex-row gap-2 items-center"
+              variant="outline"
+              disabled
+            >
+              <span>Optimizing...</span>
+              <TbReload className="w-5 h-5 text-primary animate-spin" />
+            </Button>
+          )}
+          <Button onClick={handleSave} className="w-full">
             Save
           </Button>
         </DialogFooter>
