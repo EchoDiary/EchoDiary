7d4ac112b06c733430d873e361915bf9cec091c9
Shivam77607 <shivamsharma77607@gmail.com>
2024-05-25 13:30:46 +0530

    feat: Add storage resource for image attachment functionality

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,18 +1,21 @@
-import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/resource.js';
-import { data } from './data/resource.js';
+import { storage } from "./storage/resource";
+import { defineBackend } from "@aws-amplify/backend";
 import { Stack } from "aws-cdk-lib";
+import { auth } from "./auth/resource.js";
+import { data } from "./data/resource.js";
 import { PolicyStatement } from "aws-cdk-lib/aws-iam";
 
 const backend = defineBackend({
   auth,
   data,
+  storage,
 });
 
 backend.auth.resources.authenticatedUserIamRole.addToPrincipalPolicy(
   new PolicyStatement({
     actions: [
       "transcribe:StartStreamTranscriptionWebSocket",
+      "comprehend:DetectSentiment",
     ],
     resources: ["*"],
   })
@@ -24,6 +27,17 @@ backend.addOutput({
       convert: {
         transcription: {
           defaults: {
+            language: "en-US",
+          },
+          proxy: false,
+          region: Stack.of(backend.auth.resources.authenticatedUserIamRole)
+            .region,
+        },
+      },
+      interpret: {
+        interpretText: {
+          defaults: {
+            type: "sentiment",
             language: "en-US",
           },
           proxy: false,
