6944fa92e45b80b27cdbea2c059c392f8259d738
Shivam77607 <shivamsharma77607@gmail.com>
2024-05-25 13:31:44 +0530

    feat: Update Dashboard component to display diary cards

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -1,13 +1,30 @@
-'use client';
+"use client";
 
-import { Button, Flex, Heading, Icon, Text, View } from '@aws-amplify/ui-react';
-import AddDiaryDialogButton from './AddDiaryDialogButton';
-import Navbar from './Navbar';
+import Navbar from "./Navbar";
+import DiaryCard from "./DiaryCard";
 
-function Dashboard({ signOut }: { signOut: () => void }) {
+function Dashboard({
+  signOut,
+  diaries,
+}: {
+  signOut: () => void;
+  diaries: any[];
+}) {
   return (
     <main>
       <Navbar signOut={signOut} />
+      <div className="flex flex-wrap gap-7 p-10">
+        {diaries.map((diary: any) => (
+          <DiaryCard
+            key={diary.id}
+            diaryId={diary.id}
+            content={diary.content}
+            images={diary.images}
+            mood={diary.mood}
+            date={diary.date}
+          />
+        ))}
+      </div>
     </main>
   );
 }
