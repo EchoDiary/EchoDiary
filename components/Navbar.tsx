99ad39432566a337ef05371522d915b36d0d353b
Sidharth 'R3FluX' Sreejil <94698713+sid-js@users.noreply.github.com>
2024-05-25 17:29:44 +0530

    Decrese Logo Height in Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = ({ signOut }: { signOut: () => void }) => {
   return (
     <div className='flex py-3 px-4 md:px-10 flex-row items-center justify-between bg-background border-b-2 border-secondary'>
       <div>
-        <Image src='/images/logo.png' alt='logo' width={200} height={150} />
+        <Image src='/images/logo.png' alt='logo' width={200} height={55} />
       </div>
       <div className='flex gap-2'>
         <AddDiaryDialogButton />
