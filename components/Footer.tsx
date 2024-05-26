bb97e09cf936b2db828ccc12b8b43753c6f1a7ef
Shivam77607 <shivamsharma77607@gmail.com>
2024-05-26 23:14:45 +0530

    feat: Add target _blank

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,7 +8,7 @@ function Footer() {
     <footer className="flex h-[60px] justify-center items-center bg-background text-foreground rounded-tl-lg rounded-tr-lg gap-4 relative">
       <p className="text-muted-foreground">Made with ❤️ by </p>
       <div className="flex gap-1">
-        <Link href="https://dev.to/sidjs">
+        <Link href="https://dev.to/sidjs" target="_blank">
           <Image
             src="https://media.licdn.com/dms/image/D5603AQHoR9HnW-0huw/profile-displayphoto-shrink_400_400/0/1702790621615?e=1722470400&v=beta&t=gZZK2-6SGvQynLaKZ8L8hDI2_xSkfnRSDUwtZ7MK5Cg"
             alt="Logo"
@@ -17,7 +17,10 @@ function Footer() {
             className="rounded-full object-cover hover:-translate-y-2 transition-all 200ms ease-in shadow-sm hover:shadow-lg"
           />
         </Link>
-        <Link href="https://shivam-sharma-myportfolio.vercel.app">
+        <Link
+          href="https://shivam-sharma-myportfolio.vercel.app"
+          target="_blank"
+        >
           <Image
             src="https://shivam-sharma-myportfolio.vercel.app/_next/image?url=%2Fassets%2Favatar.png&w=384&q=75"
             alt="Logo"
