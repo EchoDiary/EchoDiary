9a39aa111cd12ea5383310c45c31de974c1052ba
Shivam77607 <shivamsharma77607@gmail.com>
2024-05-26 23:12:35 +0530

    refactor: Add lazy loading for logo image in Dashboard header

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -7,6 +7,7 @@ import "@aws-amplify/ui-react/styles.css";
 import { Authenticator } from "@aws-amplify/ui-react";
 
 import Dashboard from "@/components/Dashboard";
+import Link from "next/link";
 
 Amplify.configure(outputs);
 
@@ -19,7 +20,9 @@ const components = {
   Header() {
     return (
       <div className="flex justify-center items-center pb-2">
-        <Image src="/images/logo.png" width={300} height={300} alt="logo" />
+        <Link href="/" className="cursor-pointer">
+          <Image src="/images/logo.png" width={300} height={300} alt="logo" />
+        </Link>
       </div>
     );
   },
