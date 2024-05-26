9fe0d067ba1111234a1eaf3b4d6349d1aaff918a
Shivam77607 <shivamsharma77607@gmail.com>
2024-05-26 23:11:48 +0530

    refactor: Update layout.tsx to include Footer component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,13 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import './globals.css';
-import toast, { Toaster } from 'react-hot-toast';
-const inter = Inter({ subsets: ['latin'] });
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import toast, { Toaster } from "react-hot-toast";
+import Footer from "@/components/Footer";
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'EchoDiary',
-  description: 'Your personal AI powered diary',
+  title: "EchoDiary",
+  description: "Your personal AI powered diary",
 };
 
 export default function RootLayout({
@@ -15,10 +16,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <body className={inter.className}>
         {children}
         <Toaster />
+        <Footer />
       </body>
     </html>
   );
